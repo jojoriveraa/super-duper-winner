187371465685d5231bcd2783425256110f06f261
Jorge Rivera <jariverai@gmail.com>
2019-09-13 14:33:13 -0500

    initial commit

---
+++ src/test/java/com/walmart/gtsmx/testing/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package com.walmart.gtsmx.testing;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+}
